fd06d40b7c657587965d41728b0238988b2dddc5
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-23 22:02:29 +0300

    task 25

--- seminar_4_21.02/task25/Program.cs
+++ seminar_4_21.02/task25/Program.cs
@@ -4,14 +4,19 @@
 // 3, 5 -> 243 
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+RiseToDegree( a, b);
 
-
-int x = 3;
-int degree = 5;
-int c = x^degree;
+void RiseToDegree(int a, int b)
 {
-   Console.WriteLine(c);
+    int deg = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        deg = deg * a;
+    }
+    Console.WriteLine($"Число А в натуральной степени В : {deg}");
 }
-
-
 
---
+++ seminar_4_21.02/task29/Program.cs
@@ -0,0 +1,15 @@
+﻿
+        // Задача 29: Напишите метод, который задаёт массив из N элементов 
+        // и выводит их на экран.
+        // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+        // 6, 1, 33 -> [6, 1, 33]
+        
+int elementArray = 8;
+int[] myArray = new int[elementArray];        
+for (int i = 0; i < myArray.Length; i++)
+{
+   Console.Write($"Введите элемент массива под индексом {i}: ");
+   myArray[i] = int.Parse(Console.ReadLine());
+}
+Console.Write($"Вывод массива: ");       
+Console.Write($"[{string.Join(",", myArray)}]"); 
