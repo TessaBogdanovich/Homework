c97a449bb142818b0efe89dbd97a278fa9a4c87b
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-23 12:51:24 +0300

    task 27

---
+++ seminar_4_21.02/task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и 
+// выдаёт сумму цифр в числе.
+//452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt (string message)
+{
+string ReadinPut = Console.ReadLine();
+int result = int.Parse(ReadinPut);
+return result;
+}
+
+int sumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+result += number % 10;
+number = number / 10;
+    }
+     return result;
+}
+int number = Prompt("Введите число: ");
+Console.WriteLine($"Сумма всех чисел в цифре: {number} = {sumAllDigit(number)}");
+
